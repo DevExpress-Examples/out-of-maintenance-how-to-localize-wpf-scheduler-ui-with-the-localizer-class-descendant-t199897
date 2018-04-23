495cc93cbdd5cd20e6c3273af09e5c8f6f12fc29
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LocalizerSample/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using DevExpress.Xpf.Scheduler;
+using System;
+
+namespace LocalizerSample {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Ribbon.DXRibbonWindow {
+        public MainWindow() {
+            InitializeComponent();
+
+        }
+
+        #region #CustomLocalizer
+        static MainWindow() {
+            SchedulerControlLocalizer.Active = new CustomSchedulerLocalizer();
+        }
+
+        class CustomSchedulerLocalizer : SchedulerControlLocalizer {
+            // Modify the specified caption.
+            public override string GetLocalizedString(DevExpress.Xpf.Scheduler.SchedulerControlStringId id) {
+                if (id ==SchedulerControlStringId.ButtonCaption_DismissAll)
+                    return "42";
+                else
+                    return base.GetLocalizedString(id);
+            }
+            // Localize UI strings. 
+            // Note that not all strings can be translated with Localizer classes. Use satellite assemblies in this situation. 
+            protected override void PopulateStringTable() {
+                //base.PopulateStringTable();
+                foreach (SchedulerControlStringId s in Enum.GetValues(typeof(SchedulerControlStringId))) {
+                    AddString(s, Enum.GetName(typeof(SchedulerControlStringId), s));
+                }
+            }
+        }
+        #endregion #CustomLocalizer
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to localize WPF Scheduler UI with the Localizer class descendant
+
+
+This example illustrates the use of the <a href="http://help.devexpress.com/#WPF/clsDevExpressXpfSchedulerSchedulerControlLocalizertopic">SchedulerControlLocalizer</a> class descendant to display the names of the <a href="http://help.devexpress.com/#WPF/DevExpressXpfSchedulerSchedulerControlStringIdEnumtopic">SchedulerControlStringId</a> enumeration members instead of the localized strings. This approach enables you to change most strings of the Scheduler UI as your need dictates.<br>For more information review the <a href="http://help.devexpress.com/#WPF/CustomDocument7541">WPF Controls Localization</a> topic.
+
+<br/>
+
+
